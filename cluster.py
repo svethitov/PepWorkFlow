5878669b447d2dcefb95ab9bedf881278940881a
Svetomir Hitov <svethitov@hotmail.com>
2016-11-30 11:35:42 +0100

    Should have full initial functionality

--- cluster.py
+++ cluster.py
@@ -1,8 +1,6 @@
 '''Clustering and utils for clustering used for the peptides'''
 
-from plotly.offline import plot
-import plotly.graph_objs as go
-from random import randint
+
 import numpy as np
 from sklearn.cluster import DBSCAN, MeanShift, estimate_bandwidth
 from sklearn.preprocessing import robust_scale
@@ -48,82 +46,6 @@ def listtodict(recordlist: list):
     '''Returns dictionary object from iterable'''
     return {item[0]: item[1] for item in recordlist}
 
-def plot3dscatter(recsdict: dict or list, xaxis=1, yaxis=2, zaxis=3):
-    '''Given dictionary or list filled with features vectors plot a 3d scatter plot3dscatterplot
-    You should also provide features for the different axes.
-    Mapping for axes:
-    1 - Peptide Length
-    2 - Helix %
-    3 - Strand %
-    4 - Turn %
-    5 - Disulfid bridges #
-    Uses the plotly library'''
-    if isinstance(recsdict, list):
-        recsdict = listtodict(recsdict)
-
-    vectors = [item for item in recsdict.items()]
-    data = []
-
-    # Gets lenght of Value
-    for key in recsdict.keys():
-        mykey = key
-        break
-
-    if len(recsdict[mykey]) == 6:
-        uniqueclusters = set([item[1][5] for item in vectors])
-    else:
-        uniqueclusters = [1]
-
-    for cluster in uniqueclusters:
-        if cluster < 0:
-            size = 4
-        else:
-            size = 8
-        trace = go.Scatter3d(
-            x=[item[1][xaxis - 1] for item in vectors if len(item[1]) < 6 or item[1][5] == cluster],
-            y=[item[1][yaxis - 1] for item in vectors if len(item[1]) < 6 or item[1][5] == cluster],
-            z=[item[1][zaxis - 1] for item in vectors if len(item[1]) < 6 or item[1][5] == cluster],
-            mode='markers',
-            marker=dict(
-                color='rgb(' + str(randint(0, 255)) + ',' + str(randint(0, 255)) + \
-                    ',' + str(randint(0, 255)) + ')',
-                size=size
-            ),
-            text=[item[0] for item in vectors if len(item[1]) < 6 or item[1][5] == cluster],
-            name='Cluster: {}'.format(cluster),
-            opacity=0.8
-        )
-        data.append(trace)
-
-    axisnames = {
-        1: 'Peptide length',
-        2: 'Helix %',
-        3: 'Strand %',
-        4: 'Turn %',
-        5: 'Disulfid bridges #'
-    }
-    layout = go.Layout(
-        margin=dict(
-            l=1,
-            r=1,
-            b=1,
-            t=1
-        ),
-        title='3d plot of features',
-        scene=dict(
-            xaxis=dict(
-                title=axisnames[xaxis]
-            ),
-            yaxis=dict(
-                title=axisnames[yaxis]
-            ),
-            zaxis=dict(
-                title=axisnames[zaxis]
-            )
-        )
-    )
-    fig = go.Figure(data=data, layout=layout)
-    plot(fig, filename='test_graph.html')
 
 def scale(recsdict: dict):
     '''Scales the data using Robust Scale'''
@@ -177,4 +99,4 @@ def clusterdbscan(recsdict, eps=0.5, min_samples=5):
     nclusters = len(set(labels))
     print('Number of estimated clusters: {}'.format(nclusters))
 
-    return labelclusters(recordslist, labels), nclusters
+    return labelclusters(recordslist, labels)
--- run.py
+++ run.py
@@ -1,20 +1,54 @@
+#!/usr/bin/python
 '''Workflow for Peptide clustering and alignment'''
 
 from cluster import *
 from extract import *
+from plots import plot3dscatter
 
 def main():
     '''Main function for the WorkFlow'''
     print('The script expect list.list file:')
     swissrecords = getrecords('list.list')
-    getseqstat(swissrecords)
+    getseqstat(swissrecords, 'all_records.html')
     input('Press any key to continue ...')
     pdbswissrecords = getpdb(swissrecords)
-    getseqstat(pdbswissrecords)
+    getseqstat(pdbswissrecords, 'pdb_cross-refed_records.html')
     input('Press any key to continue ...')
     savebinary('PDBRecordsSet.bin', pdbswissrecords)
-    featurevector = getfeaturesvector(pdbswissrecords)
-    
+    featuresvector = getfeaturesvector(pdbswissrecords)
+
+    eps = 1.2
+    min_samples = 2
+    while True:
+        print('Clustering will be performed with the following settings:')
+        print('eps: {}, min_samples: {}'.format(eps, min_samples))
+        answer = input('Type "no" if you want to change them:')
+        if answer == 'no':
+            eps = input('Enter new value for eps: ')
+            eps = float(eps)
+            min_samples = input('Enter new value for min_samples: ')
+            min_samples = int(min_samples)
+        print('Clustering ...')
+        clustersdict = clusterdbscan(featuresvector, eps=eps, min_samples=min_samples)
+        axes = [1, 2, 3]
+        filename = '3dscatter.html'
+        while True:
+            print(' Plotting 3D graph with the following settings:')
+            print('Name: {}'.format(filename))
+            print('x: {}, y: {}, z: {}'.format(axes[0], axes[1], axes[2]))
+            answer = input('Type "no" if you want to change them:')
+            if answer == 'no':
+                axes[0] = input('Enter value for first axis: ')
+                axes[1] = input('Enter value for second axis: ')
+                axes[2] = input('Enter value for third axis: ')
+                axes = [int(axis) for axis in axes]
+                filename = input('Enter a value for filename: ')
+
+            plot3dscatter(clustersdict, filename=filename, xaxis=axes[0],
+                          yaxis=axes[1], zaxis=axes[2])
+
+    subsetseqs(pdbseqsset=pdbswissrecords, clusterdict=clustersdict)
+
 
 if __name__ == '__main__':
     main()
